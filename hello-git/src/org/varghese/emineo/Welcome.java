843fc8912b13be6931ab2ecf0d9a3253de0657eb
varghesethomas69 <varghesethomas69@hotmail.com>
2017-09-22 23:21:26 -0500

    Committed HellGit project

---
+++ hello-git/src/org/varghese/emineo/Welcome.java
@@ -0,0 +1,12 @@
+package org.varghese.emineo;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Git From Varghese!!!!!");
+
+	}
+
+}
